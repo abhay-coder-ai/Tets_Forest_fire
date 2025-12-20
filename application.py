18416e31aa35d81df27791d722c6a5f07803573f
abhay-coder-ai <7033403893abc@gmail.com>
2025-12-20 19:26:50 +0530

    Updated app to application in application.py

--- application.py
+++ application.py
@@ -2,7 +2,8 @@ import pickle
 import numpy as np
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+application = Flask(__name__)
+app= application
 
 # Load scaler and model
 scaler = pickle.load(open("models/scaler.pkl", "rb"))
