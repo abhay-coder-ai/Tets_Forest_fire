4546a31b7ae91965e22df06cb8e2029160d29518
abhay-coder-ai <7033403893abc@gmail.com>
2025-12-30 11:44:20 +0530

    Dockerized Algerian Forest Fire ML app

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(host="0.0.0.0", port=5000)
